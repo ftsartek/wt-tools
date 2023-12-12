d8cebefac16e6af67f933c32953369bae4592937
Jordan Russell <jordan.russell@sitehost.co.nz>
2023-12-13 09:40:19 +1300

    Update requirements for Python 3.11
    Upgrading to Python 3.11 speeds things up but results in a number of incompatibilities with older module versions. We have to retain compatibility with the existing Construct version, but everything else can be upgraded.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='wt_tools',
-    author='klensy',
-    description='War Thunder resource extraction tools',
-    url='https://github.com/klensy/wt-tools',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    package_data={'wt_tools': ['formats/blk.lark']},
+    name="wt_tools",
+    author="ftsartek",
+    description="War Thunder resource extraction tools",
+    url="https://github.com/ftsartek/wt-tools",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    package_data={"wt_tools": ["formats/blk.lark"]},
 )
