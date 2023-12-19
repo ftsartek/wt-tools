896dffddaa060ef0f2f6e1c1b88673091b59b710
Jordan Russell <jordan.russell@sitehost.co.nz>
2023-12-19 13:40:41 +1300

    Extend update_checker to support normal torrent type (for automation), add return value for checking versions

--- src/wt_tools/update_checker.py
+++ src/wt_tools/update_checker.py
@@ -10,7 +10,7 @@ def main():
 
 
 @main.command(name="check")
-def check_versions_online():
+def check_versions_online() -> dict:
     """check warthunder versions online"""
     tag_list = [
         "",
@@ -34,11 +34,14 @@ def check_versions_online():
 
     s = requests.Session()
     s.headers.update(headers)
+    tags = {}
     for tag in tag_list:
         r = s.get(versions_url.format(tag))
         # if any valid version
         if r.text != "NOITEM":
             print(tag if tag else "default", r.text)
+            tags[tag if tag else "default"] = r.text
+    return tags
 
 
 @main.command(name="download")
@@ -46,8 +49,8 @@ def check_versions_online():
 def download_yup(tag):
     """download yup (torrent) file with given tag,  list of available tags in CHECK command"""
     headers = {"User-Agent": "wt-tools"}
-    yup_url = "https://yupmaster.gaijinent.com/yuitem/get_version_yup.php?proj=warthunder&tag={}"
-    r = requests.get(yup_url.format("" if tag == "default" else tag), headers=headers)
+    yup_url = f"https://yupmaster.gaijinent.com/yuitem/get_version_yup.php?proj=warthunder&tag={'' if tag == 'default' else tag}&torrent=1"
+    r = requests.get(yup_url, headers=headers)
     for line in r.text.split():
         # get the first good link
         if line.startswith("https"):
@@ -57,6 +60,20 @@ def download_yup(tag):
                 f.write(r.content)
             break
 
+
+def download_torrent(tag):
+    """download torrent file with given tag,  list of available tags in CHECK command"""
+    headers = {"User-Agent": "wt-tools"}
+    torrent_url = f"https://yupmaster.gaijinent.com/yuitem/current_yup.php?project=warthunder&torrent=1&tag={'' if tag == 'default' else tag}"
+    r = requests.get(torrent_url, headers=headers)
+    for line in r.text.split():
+        # get the first good link
+        if line.startswith("https"):
+            name = urlparse(line).path.strip("/")
+            r = requests.get(line, headers=headers)
+            with open("{}.{}".format(tag, name), "wb") as f:
+                f.write(r.content)
+            break
 
 if __name__ == "__main__":
     main()
